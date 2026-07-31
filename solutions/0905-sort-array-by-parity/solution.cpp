ccb1420dbac5b52f527949ecd775cb646ff9e0d3
Shivam Pawar <shivampawar1218@gmail.com>
2026-07-31 08:46:41 +0000

    Sync LeetCode submission - Sort Array By Parity - Runtime - 3 ms (13.84%), Memory - 20.7 MB (22.90%)

---
+++ solutions/0905-sort-array-by-parity/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        while(j<nums.size()){
+            if(nums[j]%2 == 0 ){
+                swap(nums[i], nums[j]);
+                i++;
+            }
+            j++;
+        }
+        return nums;
+    }
+};
