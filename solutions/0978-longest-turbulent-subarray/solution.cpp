a4bd2f72dfca755fb04f6414e3d73def69978719
Shivam Pawar <shivampawar1218@gmail.com>
2026-08-06 05:20:08 +0000

    Sync LeetCode submission - Longest Turbulent Subarray - Runtime - 0 ms (100.00%), Memory - 44.2 MB (61.45%)

---
+++ solutions/0978-longest-turbulent-subarray/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size();
+
+        if (n == 1) return 1;
+
+        int inc = 1;
+        int dec = 1;
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                inc = dec + 1;
+                dec = 1;
+            }
+            else if (arr[i] < arr[i - 1]) {
+                dec = inc + 1;
+                inc = 1;
+            }
+            else {
+                inc = dec = 1;
+            }
+
+            ans = max(ans, max(inc, dec));
+        }
+
+        return ans;
+    }
+};
